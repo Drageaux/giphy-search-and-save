4bd3f995a7c47142cc97760adc430258bdc326aa
David Thong Nguyen <thnguyen2808@gmail.com>
2017-05-23 11:20:42 -0400

    Got Native Base to work

--- App.js
+++ App.js
@@ -1,91 +1,45 @@
-import React from "react";
-import {AppRegistry, Dimensions, StyleSheet, Text, StatusBar, Platform} from "react-native";
-import {StyleProvider} from "@shoutem/theme";
-import {Card, Image, View, Subtitle, Caption} from "@shoutem/ui";
-import {Constants, Font, AppLoading} from "expo";
+import React, {Component} from "react";
+import {
+    AppRegistry,
+    Dimensions,
+    StyleSheet,
+    Text,
+    StatusBar,
+    Platform,
+    View
+} from "react-native";
+import Expo from "expo";
+import {
+    Container,
+    Header,
+    Body,
+    Button,
+    Title,
+    Left,
+    Right,
+    Icon
+} from "native-base";
 
 
 const window = Dimensions.get('window');
 
 const Colors = {
-    BACKGROUND: '#ffffff',
+    BACKGROUND: '#FFF',
     SHADOW: '#000000',
-};
-
-const MEDIUM_GUTTER = 15;
-
-const theme = {
-    'shoutem.ui.View': {
-        '.h-center': {
-            alignItems: 'center',
-        },
-
-        '.v-center': {
-            justifyContent: 'center',
-        },
-
-        '.flexible': {
-            flex: 1,
-        },
-
-        flexDirection: 'column',
-    },
-
-    'shoutem.ui.Card': {
-        'shoutem.ui.View.content': {
-            'shoutem.ui.Subtitle': {
-                marginBottom: MEDIUM_GUTTER,
-            },
-
-            flex: 1,
-            alignSelf: 'stretch',
-            padding: 10,
-        },
-
-        width: (180 / 375) * window.width,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        backgroundColor: Colors.BACKGROUND,
-        borderRadius: 2,
-        shadowColor: Colors.SHADOW,
-        shadowOpacity: 0.1,
-        shadowOffset: {width: 1, height: 1},
-    },
-
-    'shoutem.ui.Image': {
-        '.medium-wide': {
-            width: (180 / 375) * window.width,
-            height: 85,
-        },
-
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        resizeMode: 'cover',
-        backgroundColor: Colors.BACKGROUND,
-    },
+    RED: '#FF615C',
+    DARKRED: '#AD4047',
 };
 
 
-export default class App extends React.Component {
+export default class App extends Component {
     state = {
         fontsAreLoaded: false,
     }
 
     async componentWillMount() {
-        await Font.loadAsync({
-            'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
-            'Rubik-BlackItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
-            'Rubik-Bold': require('./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
-            'Rubik-BoldItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf'),
-            'Rubik-Italic': require('./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf'),
-            'Rubik-Light': require('./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
-            'Rubik-LightItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf'),
-            'Rubik-Medium': require('./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
-            'Rubik-MediumItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
-            'Rubik-Regular': require('./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
-            'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
+        await Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
         });
 
         this.setState({fontsAreLoaded: true});
@@ -93,42 +47,38 @@ export default class App extends React.Component {
 
     render() {
         if (!this.state.fontsAreLoaded) {
-            return <AppLoading />;
+            return <Expo.AppLoading />;
         }
         return (
-            <StyleProvider style={theme}>
-                {/*<View style={styles.container}>*/}
-                {/*<View style={styles.statusBar}>*/}
-                {/*<StatusBar backgroundColor='#AD4047' barStyle="light-content"/>*/}
-                {/*</View>*/}
-                {/*<NavigationBar leftComponent={<Icon name='sidebar'/>}*/}
-                {/*centerComponent={<Title>TITLE</Title>}/>*/}
-                {/*<View style={styles.content}/>*/}
-                {/*</View>*/}
-                <View styleName="flexible vertical v-center h-center">
-                    <Card>
-                        <Image
-                            styleName="medium-wide"
-                            source={{uri: 'http://shoutem.github.io/img/ui-toolkit/examples/image-12.png'}}
-                        />
-                        <View styleName="content">
-                            <Subtitle numberOfLines={4}>
-                                Lady Gaga Sings National Anthem at Super Bowl 50
-                            </Subtitle>
-                            <Caption>21 hours ago</Caption>
-                        </View>
-                    </Card>
+            <Container>
+                <View style={styles.statusBar}>
+                    <StatusBar backgroundColor='#AD4047' barStyle="light-content"/>
                 </View>
-            </StyleProvider>
+                <Header style={StyleSheet.flatten(styles.appBar)}>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='arrow-back'/>
+                        </Button>
+                    </Left>
+                    <Body>
+                    <Title>Header</Title>
+                    </Body>
+                    <Right>
+                        <Button transparent>
+                            <Icon name='menu'/>
+                        </Button>
+                    </Right>
+                </Header>
+            </Container>
         )
     }
 }
 
 
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : Constants.statusBarHeight;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : Expo.Constants.statusBarHeight;
 const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
-const APPBAR_MARGIN_TOP = Platform.OS === 'ios' ? 0 : Constants.statusBarHeight;
-console.log(STATUSBAR_HEIGHT, APPBAR_HEIGHT, APPBAR_MARGIN_TOP)
+const APPBAR_MARGIN_TOP = Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight;
+console.log(STATUSBAR_HEIGHT, APPBAR_HEIGHT, APPBAR_MARGIN_TOP);
 
 
 const styles = StyleSheet.create({
@@ -137,18 +87,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF'
     },
     statusBar: {
-        backgroundColor: '#AD4047',
-        height: 20,
+        backgroundColor: Colors.DARKRED,
+        height: STATUSBAR_HEIGHT,
     },
     appBar: {
-        backgroundColor: '#FF615C',
-        height: 56,
-        marginTop: APPBAR_MARGIN_TOP
+        backgroundColor: Colors.RED,
+        height: APPBAR_HEIGHT,
+        // marginTop: APPBAR_MARGIN_TOP
     },
     content: {
         flex: 1
-    },
+    }
 });
 
-// connect the component to the theme
 AppRegistry.registerComponent('App', () => App);
